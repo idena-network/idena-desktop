ae8b541a6644add2a205d77e3e8f5968d27677b0
aidenaio <a@idena.io>
2019-03-26 22:28:48 +0500

    Add babel-eslint plugin

--- renderer/.eslintrc.js
+++ renderer/.eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -15,9 +16,6 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-  ],
-  rules: {
-  },
-};
+  plugins: ['react'],
+  rules: {},
+}
