245271740101195efcf4880187bb47a93cfa1d7e
aidenaio <a@idena.io>
2019-05-22 14:29:37 +0500

    Find free port for express

--- main/index.js
+++ main/index.js
@@ -4,12 +4,14 @@ const {BrowserWindow, app, ipcMain, Tray, Menu} = require('electron')
 const isDev = require('electron-is-dev')
 const prepareNext = require('electron-next')
 const express = require('express')
+const net = require('net')
 
 const {IMAGE_SEARCH_TOGGLE, IMAGE_SEARCH_PICK} = require('./channels')
 const {startNode} = require('./idenaNode')
 
 let mainWindow
 let tray
+let expressPort = 3051
 
 // Possible values are: 'darwin', 'freebsd', 'linux', 'sunos' or 'win32'
 const isWin = process.platform === 'win32'
@@ -53,6 +55,11 @@ const createMainWindow = () => {
   })
 }
 
+const showMainWindow = () => {
+  mainWindow.show()
+  mainWindow.focus()
+}
+
 const createTray = () => {
   tray = new Tray(resolve(__dirname, 'static', 'tray', 'icon-dark-2.png'))
 
@@ -72,14 +79,14 @@ const createTray = () => {
       label: 'Quit',
       accelerator: 'Command+Q',
       selector: 'terminate:',
+      click: () => {
+        if (isWin) {
+          app.quit()
+        }
+      },
     },
   ])
   tray.setContextMenu(contextMenu)
-}
-
-const showMainWindow = () => {
-  mainWindow.show()
-  mainWindow.focus()
 }
 
 // Prepare the renderer once the app is ready
@@ -112,15 +119,50 @@ ipcMain.on('node-log', ({sender}, message) => {
   sender.send('node-log', message)
 })
 
-const server = express()
-server.get('/', (_, res) => {
-  res.sendFile(resolve(__dirname, './server.html'))
-})
+function checkPort(port) {
+  console.log('checking', port)
+  return new Promise((res, rej) => {
+    const tempServer = net.createServer()
 
-server.listen(3000, () => {
-  // eslint-disable-next-line no-console
-  console.log('Running on localhost:3000')
-})
+    tempServer.once('error', () => {
+      rej()
+    })
+
+    tempServer.once('listening', () => {
+      tempServer.close()
+      res()
+    })
+
+    tempServer.listen(port)
+  })
+}
+
+function runExpress(port) {
+  const server = express()
+  server.get('/', (_, res) => {
+    res.sendFile(resolve(__dirname, './server.html'))
+  })
+
+  server.listen(port, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Running on localhost:${port}`)
+  })
+}
+
+function choosePort() {
+  return checkPort(expressPort)
+    .then(() => {
+      console.log(`Found free port ${expressPort}`)
+      runExpress(expressPort)
+      return Promise.resolve()
+    })
+    .catch(() => {
+      expressPort += 1
+      return choosePort()
+    })
+}
+
+choosePort()
 
 let searchWindow
 ipcMain.on(IMAGE_SEARCH_TOGGLE, (event, message) => {
@@ -133,9 +175,9 @@ ipcMain.on(IMAGE_SEARCH_TOGGLE, (event, message) => {
         nodeIntegration: false,
         preload: join(__dirname, 'preload.js'),
       },
+      parent: mainWindow,
     })
-    searchWindow.setParentWindow(mainWindow)
-    searchWindow.loadURL('http://localhost:3000/')
+    searchWindow.loadURL(`http://localhost:${expressPort}/`)
     if (isDev && false) {
       searchWindow.webContents.openDevTools({
         mode: 'detach',
