384896e0f42aca8f1fb3fea528991014de70dd1e
aidenaio <a@idena.io>
2019-06-26 18:08:26 +0500

    Remove unused code for invites

--- renderer/shared/providers/invite-context.js
+++ renderer/shared/providers/invite-context.js
@@ -30,25 +30,14 @@ function InviteProvider({children}) {
 
   useInterval(
     async () => {
-      // const {hash: inviteHash} = getLastInvite()
-      // const {result: inviteResult} = await api.fetchTx(inviteHash)
-      // if (inviteResult) {
-      //   if (inviteResult.blockHash !== HASH_IN_MEMPOOL) {
-      const {result: activateResult} = await api.activateInvite(
-        address,
-        activationCode
-      )
-      if (activateResult) {
-        const {hash: activateHash} = activateResult
+      const {result} = await api.activateInvite(address, activationCode)
+      if (result) {
+        const {hash: activateHash} = result
         setActivationTx(activateHash)
         db.setActivationTx(activateHash)
       } else {
         resetActivation()
       }
-      //   }
-      // } else {
-      //   resetActivation()
-      // }
     },
     activationCode && !activationTx ? 3000 : null
   )
