6928e8e0829b96907eab4e779fca7c7b20bb848c
aidenaio <a@idena.io>
2019-08-11 16:38:29 +0500

    Refetch timing every minute

--- renderer/shared/hooks/use-timing.js
+++ renderer/shared/hooks/use-timing.js
@@ -3,24 +3,6 @@ import {fetchCeremonyIntervals} from '../api'
 import {useInterval} from './use-interval'
 import {logConnectivityIssue} from '../utils/log'
 
-async function fetchTiming() {
-  const {
-    ValidationInterval: validation,
-    FlipLotteryDuration: flipLottery,
-    ShortSessionDuration: shortSession,
-    LongSessionDuration: longSession,
-    AfterLongSessionDuration: afterLongSession,
-  } = await fetchCeremonyIntervals()
-
-  return {
-    validation,
-    flipLottery,
-    shortSession,
-    longSession,
-    afterLongSession,
-  }
-}
-
 const initialTiming = {
   validation: null,
   flipLottery: null,
@@ -34,39 +16,32 @@ function useTiming() {
   const [timing, setTiming] = React.useState(initialTiming)
   const [interval, setInterval] = React.useState(null)
 
-  React.useEffect(() => {
-    let ignore = false
-
-    async function fetchData() {
+  useInterval(
+    async () => {
       try {
-        // eslint-disable-next-line no-shadow
-        const timing = await fetchTiming()
-        if (!ignore) {
-          setTiming(timing)
-        }
+        const {
+          ValidationInterval: validation,
+          FlipLotteryDuration: flipLottery,
+          ShortSessionDuration: shortSession,
+          LongSessionDuration: longSession,
+          AfterLongSessionDuration: afterLongSession,
+        } = await fetchCeremonyIntervals()
+
+        setTiming({
+          validation,
+          flipLottery,
+          shortSession,
+          longSession,
+          afterLongSession,
+        })
+        setInterval(1000 * 60 * 1)
       } catch (error) {
-        logConnectivityIssue('timing (initial)', error)
-        setInterval(5000)
+        logConnectivityIssue('timing', error)
       }
-    }
-
-    fetchData()
-
-    return () => {
-      ignore = true
-    }
-  }, [])
-
-  useInterval(async () => {
-    try {
-      // eslint-disable-next-line no-shadow
-      const timing = await fetchTiming()
-      setTiming(timing)
-      setInterval(1000 * 60 * 1)
-    } catch (error) {
-      logConnectivityIssue('timing (poll)', error)
-    }
-  }, interval)
+    },
+    interval,
+    true
+  )
 
   return timing
 }
