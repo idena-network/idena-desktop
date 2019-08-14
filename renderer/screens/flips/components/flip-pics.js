30afc5bbdfa4ebc4603434d3ba507c061fddd688
aidenaio <a@idena.io>
2019-08-14 19:58:18 +0500

    Show hint for locally uploaded images
    Remove redundant code at flips/new page

--- renderer/screens/flips/components/flip-pics.js
+++ renderer/screens/flips/components/flip-pics.js
@@ -1,24 +1,16 @@
 import React, {useState, useEffect} from 'react'
 import PropTypes from 'prop-types'
 import {rem, position, borderRadius, margin} from 'polished'
-import {FiSearch, FiMove} from 'react-icons/fi'
-import {Box, Input, Absolute} from '../../../shared/components'
+import {FiSearch} from 'react-icons/fi'
+import {Draggable, DragDropContext, Droppable} from 'react-beautiful-dnd'
+import {Box, Input} from '../../../shared/components'
 import Divider from '../../../shared/components/divider'
 import Flex from '../../../shared/components/flex'
 import ImageEditor from './image-editor'
 import theme from '../../../shared/theme'
 import {convertToBase64Url} from '../utils/use-data-url'
 import {IMAGE_SEARCH_PICK, IMAGE_SEARCH_TOGGLE} from '../../../../main/channels'
-import FlipImage from './flip-image'
 import {IconButton} from '../../../shared/components/button'
-
-import {
-  backgrounds,
-  padding
-} from 'polished'
-import {Draggable, DragDropContext, Droppable} from 'react-beautiful-dnd'
-
-
 
 const reorder = (list, startIndex, endIndex) => {
   const result = Array.from(list)
@@ -26,8 +18,6 @@ const reorder = (list, startIndex, endIndex) => {
   result.splice(endIndex, 0, removed)
   return result
 }
-
-
 
 function FlipPics({pics, onUpdateFlip}) {
   const [selectedIndex, setSelectedIndex] = useState(0)
@@ -82,7 +72,6 @@ function FlipPics({pics, onUpdateFlip}) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [pickedUrl, selectedIndex])
 
-
   function onDragEnd(result) {
     if (!result.destination) {
       return
@@ -100,69 +89,59 @@ function FlipPics({pics, onUpdateFlip}) {
       result.destination.index
     )
 
-    onUpdateFlip(nextOrder);
+    onUpdateFlip(nextOrder)
   }
-
-
-
 
   return (
     <Flex>
-
-     <Box css={margin(0, rem(40), 0)}>
+      <Box css={margin(0, rem(40), 0)}>
         <DragDropContext onDragEnd={onDragEnd}>
           <Droppable droppableId="flip">
             {provided => (
               <div ref={provided.innerRef} {...provided.droppableProps}>
                 {pics.map((src, idx) => {
-                        const isCurrent = idx === selectedIndex
+                  const isCurrent = idx === selectedIndex
 
-                        let style=position('relative')
+                  let style = position('relative')
 
-                        if (idx === 0) {
-                          style = {...style, ...borderRadius('top', rem(8))}
-                        }
-                        if (idx === pics.length - 1) {
-                          style = {...style, ...borderRadius('bottom', rem(8))}
-                        }
+                  if (idx === 0) {
+                    style = {...style, ...borderRadius('top', rem(8))}
+                  }
+                  if (idx === pics.length - 1) {
+                    style = {...style, ...borderRadius('bottom', rem(8))}
+                  }
 
-                        if (isCurrent) {
-                                style = {
-                                  ...style,
-                                  border: `solid 2px ${theme.colors.primary}`,
-                                  boxShadow: '0 0 4px 4px rgba(87, 143, 255, 0.25)',
-                               }
-                        }
-      
+                  if (isCurrent) {
+                    style = {
+                      ...style,
+                      border: `solid 2px ${theme.colors.primary}`,
+                      boxShadow: '0 0 4px 4px rgba(87, 143, 255, 0.25)',
+                    }
+                  }
+
                   return (
-                  <Draggable key={idx} draggableId={`pic${idx}`} index={idx}>
-                    {/* eslint-disable-next-line no-shadow */}
-                    {provided => (
-                      <div
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                                        	
-                        onClick={() => {
-                          setSelectedIndex(idx)
-                        }}
-                      >
-                        <Image
-                          key={idx}
-                          src={src}
-                          style={style}
+                    <Draggable key={idx} draggableId={`pic${idx}`} index={idx}>
+                      {/* eslint-disable-next-line no-shadow */}
+                      {provided => (
+                        <div
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          onClick={() => {
+                            setSelectedIndex(idx)
+                          }}
                         >
-                        </Image>
-                      </div>
-                    )}
-                  </Draggable>
-                )})}
+                          <Image key={idx} src={src} style={style} />
+                        </div>
+                      )}
+                    </Draggable>
+                  )
+                })}
               </div>
             )}
           </Droppable>
         </DragDropContext>
       </Box>
-
 
       <Box>
         <ImageEditor src={pics[selectedIndex]} />
@@ -180,13 +159,16 @@ function FlipPics({pics, onUpdateFlip}) {
             Search on Google
           </IconButton>
           <Divider vertical />
-          <Input
-            type="file"
-            accept="image/*"
-            style={{border: 'none'}}
-            onChange={handleUpload}
-          />
-        </Flex>                                                                  	
+          <Box>
+            <Input
+              type="file"
+              accept="image/*"
+              style={{border: 'none', paddingRight: 0, width: rem(230)}}
+              onChange={handleUpload}
+            />
+            <small>(Max 150kb)</small>
+          </Box>
+        </Flex>
       </Box>
     </Flex>
   )
@@ -197,35 +179,14 @@ FlipPics.propTypes = {
   onUpdateFlip: PropTypes.func.isRequired,
 }
 
-
+// eslint-disable-next-line react/prop-types
 function Image({src, style, children}) {
   return (
     <Box>
-      <img alt="flip" width={120} src={src} style={style}/>
+      <img alt="flip" width={120} src={src} style={style} />
       {children}
     </Box>
   )
 }
-
-
-function Movable(props) {
-  return (
-    <Absolute
-      top={rem(4)}
-      right={rem(4)}
-      css={{
-        ...backgrounds(theme.colors.primary2),
-        ...padding(rem(theme.spacings.small8)),
-        ...borderRadius('top', rem(6)),
-        ...borderRadius('bottom', rem(6)),
-        opacity: 0.8,
-      }}
-      {...props}
-    >
-      <FiMove color={theme.colors.white} />
-    </Absolute>
-  )
-}
-
 
 export default FlipPics
