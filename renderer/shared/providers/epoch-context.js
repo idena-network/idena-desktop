9fd6800f5271839e150ee6782bd90493720eb93c
aidenaio <a@idena.io>
2019-08-17 15:23:30 +0500

    Poll epoch every second

--- renderer/shared/providers/epoch-context.js
+++ renderer/shared/providers/epoch-context.js
@@ -1,13 +1,8 @@
 import React from 'react'
-import dayjs from 'dayjs'
 import deepEqual from 'dequal'
 import {useInterval} from '../hooks/use-interval'
 import {fetchEpoch} from '../api'
-import {useTimingState} from './timing-context'
 import {logConnectivityIssue} from '../utils/log'
-
-const hasValues = obj => Object.values(obj).every(x => x)
-const GAP = 60
 
 export const EpochPeriod = {
   FlipLottery: 'FlipLottery',
@@ -23,9 +18,7 @@ const EpochDispatchContext = React.createContext()
 // eslint-disable-next-line react/prop-types
 function EpochProvider({children}) {
   const [epoch, setEpoch] = React.useState(null)
-  const [interval, setInterval] = React.useState(null)
-
-  const timing = useTimingState()
+  const [interval, setInterval] = React.useState(1000 * 1)
 
   React.useEffect(() => {
     let ignore = false
@@ -33,14 +26,15 @@ function EpochProvider({children}) {
     async function fetchData() {
       try {
         // eslint-disable-next-line no-shadow
-        const epoch = await fetchEpoch()
+        const nextEpoch = await fetchEpoch()
         if (!ignore) {
-          setEpoch(epoch)
-          setInterval(100)
+          setEpoch(nextEpoch)
         }
       } catch (error) {
         logConnectivityIssue('epoch (initial)', error)
-        setInterval(5000)
+        if (!ignore) {
+          setInterval(5000)
+        }
       }
     }
 
@@ -54,41 +48,6 @@ function EpochProvider({children}) {
   useInterval(async () => {
     try {
       const nextEpoch = await fetchEpoch()
-      if (hasValues(timing)) {
-        const {currentValidationStart, nextValidation} = nextEpoch
-        const {
-          flipLottery,
-          shortSession,
-          longSession,
-          afterLongSession,
-        } = timing
-
-        const validationStart = dayjs(currentValidationStart || nextValidation)
-
-        const secondsBeforeValidation = validationStart.diff(dayjs(), 's')
-        const secondsAfterValidationStart = dayjs().diff(validationStart, 's')
-
-        const isValidationSoon =
-          secondsBeforeValidation > 0 &&
-          secondsBeforeValidation < flipLottery + GAP
-
-        const isValidationRunning =
-          secondsAfterValidationStart >= 0 &&
-          secondsAfterValidationStart <
-            shortSession + longSession + afterLongSession + GAP
-
-        // eslint-disable-next-line no-shadow
-        const interval =
-          isValidationSoon || isValidationRunning
-            ? 1000
-            : Math.max(
-                validationStart.subtract(flipLottery, 's').diff(dayjs(), 'ms'),
-                1000
-              )
-
-        setInterval(interval)
-      }
-
       if (!deepEqual(epoch, nextEpoch)) {
         setEpoch(nextEpoch)
       }
