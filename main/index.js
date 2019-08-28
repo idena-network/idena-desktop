2b778fd4773d7e5eacbc293341028011c0bc829d
ridenaio <r@idena.io>
2019-08-28 20:26:36 +0500

    Open idena from tray

--- main/index.js
+++ main/index.js
@@ -37,6 +37,7 @@ app.on('second-instance', () => {
   // Someone tried to run a second instance, we should focus our window.
   if (mainWindow) {
     if (mainWindow.isMinimized()) mainWindow.restore()
+    mainWindow.show()
     mainWindow.focus()
   }
 })
