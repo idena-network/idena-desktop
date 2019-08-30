b4ddc9ef5eb1d3b7b7cf3c53da75e598913e4192
aidenaio <a@idena.io>
2019-08-30 17:19:06 +0500

    Remove unused parts

--- renderer/shared/utils/obj.js
+++ renderer/shared/utils/obj.js
@@ -10,32 +10,4 @@ const shallowCompare = (obj1, obj2) =>
   Object.keys(obj1).length === Object.keys(obj2).length &&
   Object.keys(obj1).every(key => obj1[key] === obj2[key])
 
-export function removeKeys(obj, keys) {
-  if (!obj) return obj
-  return Object.keys(obj).reduce((prev, key) => {
-    switch (typeof obj[key]) {
-      case 'object': {
-        if (Array.isArray(obj[key])) {
-          prev[key] = obj[key].map(item => removeKeys(item, keys))
-        } else {
-          const index = keys.indexOf(key)
-          if (index === -1) {
-            prev[key] = removeKeys(obj[key], keys)
-          }
-        }
-        break
-      }
-      default: {
-        const index = keys.indexOf(key)
-        if (index === -1) {
-          prev[key] = obj[key]
-          return prev
-        }
-        break
-      }
-    }
-    return prev
-  }, {})
-}
-
 export {strip, shallowCompare}
