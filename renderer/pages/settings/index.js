06ba5ba0fca8b7055be6d463d84197b503bd1d95
aidenaio <50833934+aidenaio@users.noreply.github.com>
2019-10-03 19:22:09 +0500

    Export PK with password (#30)
    * Display QR code after exporting PK
    
    * Display pk in a dialog
    
    * Prevent flickering of prev pk
    
    * Make it submittable with form
    
    Prevent edits while pk dialog is open

--- renderer/pages/settings/index.js
+++ renderer/pages/settings/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {margin, rem} from 'polished'
+import QRCode from 'qrcode.react'
 
 import Layout from '../../shared/components/layout'
 import {
@@ -9,8 +10,10 @@ import {
   Input,
   Label,
   Button,
+  Modal,
+  Text,
+  Field,
 } from '../../shared/components'
-import Link from '../../shared/components/link'
 import theme from '../../shared/theme'
 import {FlatButton} from '../../shared/components/button'
 import Divider from '../../shared/components/divider'
@@ -20,6 +23,7 @@ import {useNotificationDispatch} from '../../shared/providers/notification-conte
 import {usePersistence} from '../../shared/hooks/use-persistent-state'
 import {BASE_API_URL} from '../../shared/api/api-client'
 import {loadState} from '../../shared/utils/persist'
+import useRpc from '../../shared/hooks/use-rpc'
 
 const {clear: clearFlips} = global.flipStore || {}
 const inviteDb = global.invitesDb || {}
@@ -90,10 +94,7 @@ function Settings() {
         </Box>
         {global.isDev && (
           <>
-            <Box my={rem(theme.spacings.medium32)}>
-              <SubHeading css={margin(0, 0, theme.spacings.small, 0)}>
-                Flips
-              </SubHeading>
+            <Section title="Flips">
               <Box>
                 <Button
                   onClick={() => {
@@ -114,11 +115,8 @@ function Settings() {
                   Archive flips
                 </Button>
               </Box>
-            </Box>
-            <Box my={rem(theme.spacings.medium32)}>
-              <SubHeading css={margin(0, 0, theme.spacings.small, 0)}>
-                Invites
-              </SubHeading>
+            </Section>
+            <Section title="Invites">
               <Box my={theme.spacings.small}>
                 <Button
                   onClick={() => {
@@ -129,22 +127,99 @@ function Settings() {
                   Clear invites
                 </Button>
               </Box>
-            </Box>
-            <Box my={rem(theme.spacings.medium32)}>
-              <SubHeading css={margin(0, 0, theme.spacings.small, 0)}>
-                Validation
-              </SubHeading>
-              <Box my={theme.spacings.small}>
-                <Link href="/validation/short">Short</Link>
-              </Box>
-              <Box my={theme.spacings.small}>
-                <Link href="/validation/long">Long</Link>
-              </Box>
-            </Box>
+            </Section>
+            <ExportPK />
           </>
         )}
       </Box>
     </Layout>
+  )
+}
+
+// eslint-disable-next-line react/prop-types
+function Section({title, children}) {
+  return (
+    <Box my={rem(theme.spacings.medium32)}>
+      <SubHeading css={margin(0, 0, theme.spacings.small, 0)}>
+        {title}
+      </SubHeading>
+      <Box my={rem(theme.spacings.small8)}>{children}</Box>
+    </Box>
+  )
+}
+
+function ExportPK() {
+  const [{result: pk}, callRpc] = useRpc()
+  const [password, setPassword] = React.useState()
+  const [showDialog, setShowDialog] = React.useState()
+  React.useEffect(() => setShowDialog(!!pk), [pk])
+  return (
+    <Section title="Export PK">
+      <form
+        onSubmit={e => {
+          e.preventDefault()
+          callRpc('dna_exportKey', password)
+        }}
+      >
+        <Label htmlFor="url">Password</Label>
+        <Flex align="center">
+          <Input
+            value={password}
+            type="password"
+            style={{
+              ...margin(0, theme.spacings.normal, 0, 0),
+              width: rem(300),
+            }}
+            disabled={showDialog}
+            onChange={e => setPassword(e.target.value)}
+          />
+          <Button type="submit" disabled={!password}>
+            Export PK
+          </Button>
+        </Flex>
+      </form>
+      <PkDialog show={showDialog} onHide={() => setShowDialog(false)}>
+        <Box
+          css={{
+            ...margin(rem(theme.spacings.medium24)),
+            textAlign: 'center',
+          }}
+        >
+          <QRCode value={pk} />
+        </Box>
+        <Box>
+          <Field
+            id="pk"
+            label="Encrypted private key"
+            defaultValue={pk}
+            readonly
+            disabled
+            allowCopy
+          />
+        </Box>
+      </PkDialog>
+    </Section>
+  )
+}
+
+// eslint-disable-next-line react/prop-types
+function PkDialog({children, onHide, ...props}) {
+  return (
+    <Modal onHide={onHide} {...props}>
+      <Box m="0 0 18px">
+        <SubHeading>Encrypted private key</SubHeading>
+        <Text>
+          Scan QR by your mobile phone or copy code below for export private
+          key.
+        </Text>
+        {children}
+      </Box>
+      <Flex align="center" justify="flex-end">
+        <Button variant="secondary" onClick={onHide}>
+          Close
+        </Button>
+      </Flex>
+    </Modal>
   )
 }
 
