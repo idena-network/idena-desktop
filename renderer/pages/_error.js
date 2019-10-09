d1bf29b4acb64f8774fae93092b3161a011f6a07
aidenaio <a@idena.io>
2019-10-09 20:38:22 +0500

    Remove explicit reload button from Error page

--- renderer/pages/_error.js
+++ renderer/pages/_error.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Layout from '../shared/components/layout'
-import {Box, Button} from '../shared/components'
+import {Box} from '../shared/components'
 
 class Error extends React.Component {
   static getInitialProps({res, err}) {
@@ -18,11 +18,6 @@ class Error extends React.Component {
           ? `An error ${statusCode} occurred on server`
           : 'An error occurred on client'}
         <Box>{err ? JSON.stringify(err) : 'Weird, no error caught'}</Box>
-        <Box>
-          <Button onClick={() => window.location.reload(true)}>
-            Reload the page
-          </Button>
-        </Box>
       </Layout>
     )
   }
