1a64311dc06300acc2aada9621c8527eb8c22eb1
aidenaio <a@idena.io>
2020-01-23 17:32:38 +0500

    Allow to enter validation only at short session

--- renderer/screens/validation/components/banner.js
+++ renderer/screens/validation/components/banner.js
@@ -117,7 +117,7 @@ function ValidationRunning() {
           </Box>
         )}
       </Flex>
-      {!hasAnswers && !!seconds && canValidate(identity) && (
+      {isShortSession && !hasAnswers && !!seconds && canValidate(identity) && (
         <Flex css={padding(theme.spacings.normal)}>
           <Divider vertical m={rem(theme.spacings.medium16)} />
           <Link href="/validation">
