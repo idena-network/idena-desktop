c253279363f0ecc43feec3bbde10cb3ec2a810a2
midenaio <m@idena.io>
2020-01-29 22:50:12 +0500

    Check node version with timeout

--- main/index.js
+++ main/index.js
@@ -15,6 +15,7 @@ const express = require('express')
 const net = require('net')
 const loadRoute = require('./utils/routes')
 const logger = require('./logger')
+const {promiseTimeout} = require('./utils')
 
 logger.info('idena started', global.appVersion || app.getVersion())
 
@@ -321,7 +322,7 @@ ipcMain.on(NODE_COMMAND, async (event, command, data) => {
 
   switch (command) {
     case 'init-local-node': {
-      getCurrentVersion()
+      promiseTimeout(3000, getCurrentVersion())
         .then(version => {
           sendMainWindowMsg(NODE_EVENT, 'node-ready', version)
         })
