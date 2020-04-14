3c04cf21737f32fad7adac6012720123d5ac4ffe
aidenaio <a@idena.io>
2020-04-15 00:50:30 +0500

    Add Pay for banner drawer

--- renderer/pages/ads/list.js
+++ renderer/pages/ads/list.js
@@ -1,7 +1,26 @@
 import React from 'react'
-import {Box, Flex, Stack, Text, Divider} from '@chakra-ui/core'
+import {
+  Box,
+  Flex,
+  Stack,
+  Text,
+  Divider,
+  MenuDivider,
+  Drawer,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerBody,
+  useDisclosure,
+  DrawerContent,
+  DrawerOverlay,
+  DrawerCloseButton,
+  Heading,
+  Icon,
+  NumberInput,
+  FormLabel,
+  FormControl,
+} from '@chakra-ui/core'
 import {useMachine} from '@xstate/react'
-import {FiMoreVertical} from 'react-icons/fi'
 import NextLink from 'next/link'
 import dayjs from 'dayjs'
 import {linearGradient, transparentize, cover} from 'polished'
@@ -19,13 +38,20 @@ import {
   AdTargeting,
   SmallFigureLabel,
   SmallFigureNumber,
+  AdMenu,
+  AdMenuItem,
+  AdMenuItemIcon,
 } from '../../screens/ads/components'
 import {useIdentityState} from '../../shared/providers/identity-context'
 import {add} from '../../shared/utils/math'
 import {rem} from '../../shared/theme'
 import Layout from '../../shared/components/layout'
 import {Page, PageTitle} from '../../screens/app/components'
-import {SecondaryButton, IconButton} from '../../shared/components'
+import {
+  SecondaryButton,
+  IconButton,
+  PrimaryButton,
+} from '../../shared/components'
 import {adsMachine} from '../../screens/ads/machine'
 import {loadAds, AdStatus, adStatusColor, toDna} from '../../screens/ads/utils'
 import {persistState} from '../../shared/utils/persist'
@@ -35,7 +61,6 @@ export default function MyAds() {
     {
       context: {ads},
     },
-    send,
   ] = useMachine(
     adsMachine.withConfig(
       {
@@ -54,6 +79,8 @@ export default function MyAds() {
   const {balance} = useIdentityState()
 
   const hasPendingChanges = ads.some(({burnt}) => burnt <= 0)
+
+  const {isOpen, onOpen, onClose} = useDisclosure()
 
   return (
     <Layout>
@@ -94,7 +121,7 @@ export default function MyAds() {
               title,
               burnt: spent = 0,
               lastTx = dayjs(),
-              status = AdStatus.NotShowing,
+              status = AdStatus.Idle,
             }) => (
               <AdEntry key={title}>
                 <Flex>
@@ -103,27 +130,21 @@ export default function MyAds() {
                       <AdImage
                         src={adCover || '//placekitten.com/60/60'}
                         alt={title}
-                        {...linearGradient({
-                          colorStops: [
-                            adStatusColor(status),
-                            transparentize(0.84, adStatusColor(status)),
-                          ],
-                          fallback: 'none',
-                          toDirection: 'to top',
-                        })}
                       ></AdImage>
-                      <Box
-                        rounded="lg"
-                        {...cover()}
-                        {...linearGradient({
-                          colorStops: [
-                            adStatusColor(status),
-                            transparentize(0.84, adStatusColor(status)),
-                          ],
-                          fallback: 'none',
-                          toDirection: 'to top',
-                        })}
-                      ></Box>
+                      {status !== AdStatus.Idle && (
+                        <Box
+                          rounded="lg"
+                          {...cover()}
+                          {...linearGradient({
+                            colorStops: [
+                              adStatusColor(status),
+                              transparentize(0.84, adStatusColor(status)),
+                            ],
+                            fallback: 'none',
+                            toDirection: 'to top',
+                          })}
+                        />
+                      )}
                     </Box>
                     <Text
                       color={adStatusColor(status)}
@@ -140,9 +161,25 @@ export default function MyAds() {
                       </Text>
                       <Stack isInline align="center" spacing={4} ml="auto">
                         <Box>
-                          <FiMoreVertical />
+                          <AdMenu>
+                            <NextLink href="/ads/edit?id=1">
+                              <AdMenuItem>
+                                <AdMenuItemIcon name="edit" />
+                                Edit
+                              </AdMenuItem>
+                            </NextLink>
+                            {/* <AdMenuItem>
+                              <AdMenuItemIcon as={FiPause} />
+                              Disable
+                            </AdMenuItem> */}
+                            <MenuDivider color="gray.100" my={2} />
+                            <AdMenuItem color="red.500">
+                              <AdMenuItemIcon name="delete" color="red.500" />
+                              Delete
+                            </AdMenuItem>
+                          </AdMenu>
                         </Box>
-                        <SecondaryButton>Pay</SecondaryButton>
+                        <SecondaryButton onClick={onOpen}>Pay</SecondaryButton>
                       </Stack>
                     </Flex>
                     <Stack isInline spacing={rem(58)}>
@@ -167,26 +204,26 @@ export default function MyAds() {
                     </Stack>
                     <AdTargeting>
                       <Stack isInline spacing={2}>
-                        <Box>
+                        <Stack spacing={1}>
                           <SmallFigureLabel>Location</SmallFigureLabel>
                           <SmallFigureLabel>Language</SmallFigureLabel>
                           <SmallFigureLabel>Stake</SmallFigureLabel>
-                        </Box>
-                        <Box>
+                        </Stack>
+                        <Stack spacing={1}>
                           <SmallFigureNumber>USA</SmallFigureNumber>
                           <SmallFigureNumber>en</SmallFigureNumber>
                           <SmallFigureNumber>>1000</SmallFigureNumber>
-                        </Box>
+                        </Stack>
                       </Stack>
                       <Stack isInline spacing={2}>
-                        <Box>
+                        <Stack spacing={1}>
                           <SmallFigureLabel>Age</SmallFigureLabel>
                           <SmallFigureLabel>OS</SmallFigureLabel>
-                        </Box>
-                        <Box>
+                        </Stack>
+                        <Stack spacing={1}>
                           <SmallFigureNumber>>10</SmallFigureNumber>
                           <SmallFigureNumber>macOS</SmallFigureNumber>
-                        </Box>
+                        </Stack>
                       </Stack>
                       <Divider
                         borderColor="gray.100"
@@ -194,65 +231,104 @@ export default function MyAds() {
                         orientation="vertical"
                         opacity={1}
                       ></Divider>
-                      <Box ml={4}>
+                      <Box ml={4} mt={rem(6)}>
                         <Stack isInline spacing={2}>
-                          <Box>
+                          <Stack spacing={rem(6)}>
                             <FigureLabel>Competitors</FigureLabel>
                             <FigureLabel>Max price</FigureLabel>
-                          </Box>
-                          <Box>
+                          </Stack>
+                          <Stack spacing={rem(6)}>
                             <FigureNumber fontSize={rem(13)} fontWeight={500}>
                               1
                             </FigureNumber>
                             <FigureNumber fontSize={rem(13)} fontWeight={500}>
                               0.000000000123 DNA
                             </FigureNumber>
-                          </Box>
+                          </Stack>
                         </Stack>
                       </Box>
                     </AdTargeting>
                   </Box>
                 </Flex>
-                {/* <Menu>
-                      <MenuButton
-                        style={{
-                          backgroundColor: 'none',
-                          border: 'none',
-                        }}
-                      >
-                        <FiMoreVertical size={rem(20)} />
-                      </MenuButton>
-                      <MenuList
-                        style={{
-                          ...backgrounds(theme.colors.white),
-                          ...borderRadius('top', rem(4)),
-                          ...borderRadius('bottom', rem(4)),
-                          boxShadow:
-                            '0 4px 6px 0 rgba(83, 86, 92, 0.24), 0 0 2px 0 rgba(83, 86, 92, 0.2)',
-                          ...padding(rem(8), 0),
-                          width: rem(180),
-                        }}
-                      >
-                        <AdRowMenuItem>
-                          <AdRowMenuIcon icon={FiEdit} />
-                          Edit
-                        </AdRowMenuItem>
-                        <AdRowMenuItem>
-                          <AdRowMenuIcon icon={FiPause} />
-                          Disable
-                        </AdRowMenuItem>
-                        <AdRowMenuItem as={Divider} />
-                        <AdRowMenuItem variant="danger">
-                          <AdRowMenuIcon icon={FiDelete} variant="danger" />
-                          Delete
-                        </AdRowMenuItem>
-                      </MenuList>
-                    </Menu> */}
                 <AdEntryDivider />
               </AdEntry>
             )
           )}
         </AdList>
+        <Drawer isOpen={isOpen} onClose={onClose} size={rem(360)}>
+          <DrawerOverlay />
+          <DrawerContent px={8} py={10} size={rem(360)}>
+            <DrawerCloseButton />
+            <DrawerHeader p={0}>
+              <Box
+                display="inline-block"
+                bg="brandBlue.10"
+                rounded="lg"
+                mb={4}
+                p={3}
+                alignSelf="flex-start"
+              >
+                <Icon name="ads" size={6} color="brandBlue.500" />
+              </Box>
+              <Heading fontSize={rem(18)} fontWeight={500}>
+                Pay
+              </Heading>
+            </DrawerHeader>
+            <DrawerBody mt={2} p={0}>
+              <Text>
+                In order to make your ads visible for Idena users you need to
+                burn more coins than competitors targeting the same audience.
+              </Text>
+              <Box bg="gray.50" p={6} my={6} rounded="lg">
+                <Stack isInline spacing={rem(10)} mb={6}>
+                  <AdImage src="//placekitten.com/60" size={rem(60)}></AdImage>
+                  <Text fontWeight={500}>
+                    A new ICO is running. DNA is accepted
+                  </Text>
+                </Stack>
+                <Divider />
+                <Stack isInline spacing={6} my={rem(10)}>
+                  <Stack spacing={rem(6)} w={rem(80)}>
+                    <FigureLabel>Competitors</FigureLabel>
+                    <FigureLabel>Max price</FigureLabel>
+                  </Stack>
+                  <Stack spacing={rem(6)}>
+                    <FigureNumber fontSize={rem(13)} fontWeight={500}>
+                      1
+                    </FigureNumber>
+                    <FigureNumber fontSize={rem(13)} fontWeight={500}>
+                      0.000000000123 DNA
+                    </FigureNumber>
+                  </Stack>
+                </Stack>
+                <Divider borderWidth="1px" />
+                <Stack isInline spacing={6} mt={4}>
+                  <Stack spacing={1} w={rem(80)}>
+                    <SmallFigureLabel>Location</SmallFigureLabel>
+                    <SmallFigureLabel>Language</SmallFigureLabel>
+                    <SmallFigureLabel>Stake</SmallFigureLabel>
+                    <SmallFigureLabel>Age</SmallFigureLabel>
+                    <SmallFigureLabel>OS</SmallFigureLabel>
+                  </Stack>
+                  <Stack spacing={1}>
+                    <SmallFigureNumber>USA</SmallFigureNumber>
+                    <SmallFigureNumber>en</SmallFigureNumber>
+                    <SmallFigureNumber>>1000</SmallFigureNumber>
+                    <SmallFigureNumber>22</SmallFigureNumber>
+                    <SmallFigureNumber>macOS</SmallFigureNumber>
+                  </Stack>
+                </Stack>
+              </Box>
+              <FormControl>
+                <FormLabel htmlFor="amount">Amount, DNA</FormLabel>
+                <NumberInput id="amount"></NumberInput>
+              </FormControl>
+            </DrawerBody>
+            <DrawerFooter p={0}>
+              <PrimaryButton>Burn</PrimaryButton>
+            </DrawerFooter>
+          </DrawerContent>
+        </Drawer>
       </Page>
     </Layout>
   )
--- renderer/screens/ads/components.js
+++ renderer/screens/ads/components.js
@@ -11,16 +11,15 @@ import {
   StatLabel,
   StatNumber,
   StatGroup,
+  Menu,
+  MenuButton,
+  Icon,
+  MenuList,
+  PseudoBox,
+  MenuItem,
 } from '@chakra-ui/core'
 import {useTranslation} from 'react-i18next'
-import {
-  padding,
-  margin,
-  transparentize,
-  triangle,
-  position,
-  backgrounds,
-} from 'polished'
+import {padding, margin, triangle, position, backgrounds} from 'polished'
 import {
   TableRow,
   TableCol,
@@ -282,4 +281,50 @@ export function Triangle({size, color, bg}) {
       />
     </Absolute>
   )
+}
+
+export function AdMenu(props) {
+  return (
+    <Menu autoSelect={false}>
+      <MenuButton
+        rounded="md"
+        py={1}
+        _hover={{bg: 'gray.50'}}
+        _expanded={{bg: 'gray.50'}}
+        _focus={{outline: 0}}
+      >
+        <Icon name="more" size={5} />
+      </MenuButton>
+      <MenuList
+        placement="bottom-end"
+        border="none"
+        shadow="0 4px 6px 0 rgba(83, 86, 92, 0.24), 0 0 2px 0 rgba(83, 86, 92, 0.2)"
+        rounded="lg"
+        py={2}
+        minWidth="145px"
+        {...props}
+      />
+    </Menu>
+  )
+}
+
+export function AdMenuItem(props) {
+  return (
+    <PseudoBox
+      as={MenuItem}
+      fontWeight={500}
+      lineHeight={rem(20)}
+      px={3}
+      py={rem(6)}
+      _hover={{bg: 'gray.50'}}
+      _focus={{bg: 'gray.50'}}
+      _selected={{bg: 'gray.50'}}
+      _active={{bg: 'gray.50'}}
+      {...props}
+    ></PseudoBox>
+  )
+}
+
+export function AdMenuItemIcon(props) {
+  return <Icon size={5} mr={3} color="brandBlue.500" {...props} />
 }
