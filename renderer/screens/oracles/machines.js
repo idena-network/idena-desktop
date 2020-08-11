a013ac14449ee703aa0f2511372662e90e2e301f
aidenaio <a@idena.io>
2020-08-11 05:09:27 +0500

    Try deploying contract

--- renderer/screens/oracles/machines.js
+++ renderer/screens/oracles/machines.js
@@ -2,6 +2,7 @@ import {Machine, assign} from 'xstate'
 import {log} from 'xstate/lib/actions'
 import {fetchVotings} from './utils'
 import {VotingStatus} from '../../shared/types'
+import {callRpc} from '../../shared/utils/utils'
 
 export const votingListMachine = Machine(
   {
@@ -54,35 +55,111 @@ export const votingListMachine = Machine(
   }
 )
 
-export const createVotingMachine = () =>
-  Machine(
-    {
-      context: {},
-      initial: 'idle',
-      states: {
-        idle: {
-          on: {
-            CHANGE: {
-              target: 'dirty',
-              actions: ['onChange', log()],
-            },
+export const newVotingMachine = Machine(
+  {
+    initial: 'idle',
+    states: {
+      idle: {
+        on: {
+          CHANGE: {
+            target: 'dirty',
+            actions: ['onChange', log()],
           },
         },
-        dirty: {
-          on: {
-            CHANGE: {
-              actions: ['onChange', log()],
+      },
+      dirty: {
+        on: {
+          CHANGE: {
+            actions: ['onChange', log()],
+          },
+          PUBLISH: {
+            target: 'publishing',
+            actions: [log()],
+          },
+        },
+      },
+      publishing: {
+        initial: 'creatingVotingContent',
+        states: {
+          creatingVotingContent: {
+            invoke: {
+              src: 'createVotingContent',
+              onDone: {
+                target: 'deployingContract',
+                actions: [
+                  assign({
+                    cid: (_, cid) => cid,
+                  }),
+                ],
+              },
+            },
+          },
+          deployingContract: {
+            states: {
+              estimating: {
+                invoke: {
+                  src: 'estimateDeployContract',
+                  onDone: {
+                    target: 'deploying',
+                    actions: [
+                      assign((context, {contract: contractHash, txHash}) => ({
+                        ...context,
+                        contractHash,
+                        txHash,
+                      })),
+                    ],
+                  },
+                },
+              },
+              deploying: {
+                invoke: {
+                  src: 'deployContract',
+                  onDone: {
+                    target: 'deploying',
+                    actions: [
+                      assign((context, {contract: contractHash, txHash}) => ({
+                        ...context,
+                        contractHash,
+                        txHash,
+                      })),
+                    ],
+                  },
+                },
+              },
             },
           },
         },
       },
     },
-    {
-      actions: {
-        onChange: assign((context, {name, value}) => ({
-          ...context,
-          [name]: value,
-        })),
-      },
-    }
-  )
+  },
+  {
+    actions: {
+      onChange: assign((context, {name, value}) => ({
+        ...context,
+        [name]: value,
+      })),
+    },
+    services: {
+      createVotingContent: ({title, desc}) =>
+        callRpc('ipfs_add', {title, desc}),
+      estimateDeployContract: ({cid, startDate}) =>
+        callRpc('dna_estimateDeployContract', {
+          codehash: '0x02',
+          contractStake: 1000,
+          args: [
+            {index: 0, format: 'hex', value: cid},
+            {index: 1, format: 'uint64', value: new Date(startDate).valueOf()},
+          ],
+        }),
+      deployContract: ({cid, startDate}) =>
+        callRpc('dna_deployContract', {
+          codehash: '0x02',
+          contractStake: 1000,
+          args: [
+            {index: 0, format: 'hex', value: cid},
+            {index: 1, format: 'uint64', value: new Date(startDate).valueOf()},
+          ],
+        }),
+    },
+  }
+)
