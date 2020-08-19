9e432510085209ef31a7cbb03ffaac5aa3065935
Alexander Bus <busfromrus@gmail.com>
2020-08-19 22:47:01 +0700

    Fix copy method of text fileds
    Fixes #408

--- renderer/shared/hooks/use-clipboard.js
+++ renderer/shared/hooks/use-clipboard.js
@@ -1,18 +1,5 @@
 import React from 'react'
 
-function copyText(text) {
-  const tempInput = document.createElement('input')
-  tempInput.type = 'text'
-  tempInput.value = text
-
-  document.body.appendChild(tempInput)
-
-  tempInput.select()
-  document.execCommand('copy')
-
-  document.body.removeChild(tempInput)
-}
-
 function useClipboard() {
   const [value, setValue] = React.useState()
   const [error, setError] = React.useState()
@@ -22,7 +9,7 @@ function useClipboard() {
       throw new Error(`You must copy a string, not ${typeof text}`)
     }
     try {
-      copyText(text)
+      navigator.clipboard.writeText(text)
       setValue(text)
     } catch ({message}) {
       setError(message)
