59a9327be261c54c8160d0d4e2fb74962ac70ae0
aidenaio <a@idena.io>
2020-08-29 01:29:21 +0500

    Tune in node settings page

--- renderer/pages/settings/node.js
+++ renderer/pages/settings/node.js
@@ -1,24 +1,23 @@
-/* eslint-disable jsx-a11y/no-static-element-interactions */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, {useEffect, useReducer, useRef, useState} from 'react'
-import {margin, padding, borderRadius} from 'polished'
 import {useTranslation} from 'react-i18next'
 import Ansi from 'ansi-to-react'
 import {FiEye, FiEyeOff} from 'react-icons/fi'
 import {
   Box,
-  Input,
-  Label,
-  Button,
-  Switcher,
   Text,
-  SubHeading,
-} from '../../shared/components'
-import theme, {rem} from '../../shared/theme'
-import {FlatButton} from '../../shared/components/button'
-import Divider from '../../shared/components/divider'
-import Flex from '../../shared/components/flex'
-import {useNotificationDispatch} from '../../shared/providers/notification-context'
+  Heading,
+  Stack,
+  Divider,
+  InputRightElement,
+  InputGroup,
+  IconButton,
+  Button,
+  Flex,
+  useToast,
+} from '@chakra-ui/core'
+import {Switcher} from '../../shared/components'
+import theme from '../../shared/theme'
+import {PrimaryButton, SecondaryButton} from '../../shared/components/button'
 import {BASE_API_URL} from '../../shared/api/api-client'
 import SettingsLayout from './layout'
 import {
@@ -31,19 +30,26 @@ import {
 } from '../../shared/providers/node-context'
 import {NODE_EVENT, NODE_COMMAND} from '../../../main/channels'
 import {getLayout} from '../../screens/app/layout'
+import {FormLabel, Input, Toast} from '../../shared/components/components'
 
 function NodeSettings() {
-  const {addNotification} = useNotificationDispatch()
   const {t} = useTranslation()
+
+  const toast = useToast()
+
   const settings = useSettingsState()
+
   const {
     saveExternalUrl,
     toggleUseExternalNode,
     toggleRunInternalNode,
     saveExternalApiKey,
   } = useSettingsDispatch()
+
   const {nodeFailed} = useNodeState()
+
   const {tryRestartNode} = useNodeDispatch()
+
   const logsRef = useRef(null)
 
   const [state, dispatch] = useReducer(
@@ -119,105 +125,89 @@ function NodeSettings() {
   })
 
   const notify = () =>
-    addNotification({
-      title: t('Settings updated'),
-      body: t('Connected to url', {url: state.url}),
+    toast({
+      // eslint-disable-next-line react/display-name
+      render: () => (
+        <Toast
+          title={t('Settings updated')}
+          description={t('Connected to url', {url: state.url})}
+        />
+      ),
     })
 
   const [revealApiKey, setRevealApiKey] = useState(false)
 
   return (
-    <SettingsLayout>
-      <Box py={theme.spacings.xlarge}>
-        <Flex align="center">
-          <Box>
-            <Switcher
-              isChecked={settings.runInternalNode}
-              onChange={() => {
-                toggleRunInternalNode(!settings.runInternalNode)
-              }}
-              bgOn={theme.colors.primary}
-            />
-          </Box>
-          <div
-            style={{
-              ...margin(0, 0, 0, rem(theme.spacings.small12)),
+    <Stack spacing={8} mt={8}>
+      <Stack isInline spacing={3} align="center">
+        <Box>
+          <Switcher
+            isChecked={settings.runInternalNode}
+            onChange={() => {
+              toggleRunInternalNode(!settings.runInternalNode)
             }}
-          >
-            <strong>{t('Run built-in node')}</strong>
-            <div>{t('Use built-in node to have automatic updates')}</div>
-          </div>
-          {settings.runInternalNode && nodeFailed && (
-            <div
-              style={{
-                ...margin(0, 0, 0, rem(theme.spacings.small12)),
-              }}
-            >
-              <Text css={{color: theme.colors.warning}}>
-                {t('Node failed to start')}
-              </Text>
-              <Button
-                variant="secondary"
-                css={{marginLeft: 10}}
-                onClick={() => tryRestartNode()}
-              >
-                {t('Try restart')}
-              </Button>
-            </div>
-          )}
-        </Flex>
-      </Box>
-      <Box py={theme.spacings.large}>
-        <Flex align="center">
-          <Box>
-            <Switcher
-              isChecked={settings.useExternalNode}
-              onChange={() => {
-                toggleUseExternalNode(!settings.useExternalNode)
-              }}
-              bgOn={theme.colors.primary}
-            />
+            bgOn={theme.colors.primary}
+          />
+        </Box>
+        <Box>
+          <Text fontWeight={500}>{t('Run built-in node')}</Text>
+          <Text color="muted">
+            {t('Use built-in node to have automatic updates')}
+          </Text>
+        </Box>
+        {settings.runInternalNode && nodeFailed && (
+          <Box mb={3}>
+            <Text color="red.500">{t('Node failed to start')}</Text>
+            <SecondaryButton onClick={() => tryRestartNode()}>
+              {t('Try restart')}
+            </SecondaryButton>
           </Box>
-          <div
-            style={{
-              ...margin(0, 0, 0, rem(theme.spacings.small12)),
+        )}
+      </Stack>
+      <Stack isInline spacing={3} align="center">
+        <Box>
+          <Switcher
+            isChecked={settings.useExternalNode}
+            onChange={() => {
+              toggleUseExternalNode(!settings.useExternalNode)
             }}
-          >
-            <strong>{t('Connect to remote node')}</strong>
-            <div>
-              {t(
-                'Specify the Node address if you want to connect to remote node'
-              )}
-            </div>
-          </div>
-        </Flex>
-      </Box>
+            bgOn={theme.colors.primary}
+          />
+        </Box>
+        <Box>
+          <Text fontWeight={500}>{t('Connect to remote node')}</Text>
+          <Text color="muted">
+            {t(
+              'Specify the Node address if you want to connect to remote node'
+            )}
+          </Text>
+        </Box>
+      </Stack>
       {settings.useExternalNode && (
-        <Box py={theme.spacings.xlarge}>
-          <Flex align="center">
-            <Label htmlFor="url" style={{width: 120}}>
+        <Stack spacing={2}>
+          <Stack isInline spacing={2} align="center">
+            <FormLabel htmlFor="url" fontWeight={400} minW={24}>
               {t('Node address')}
-            </Label>
+            </FormLabel>
             <Input
               id="url"
               value={state.url}
+              w="xs"
               onChange={e => dispatch({type: 'SET_URL', data: e.target.value})}
-              style={{
-                ...margin(0, theme.spacings.normal, 0, theme.spacings.small),
-                width: rem(300),
-              }}
             />
-            <Button
+            <PrimaryButton
               onClick={() => {
                 saveExternalUrl(state.url)
                 notify()
               }}
             >
               {t('Save')}
-            </Button>
-            <Divider vertical m={theme.spacings.small} />
-            <FlatButton
-              color={theme.colors.primary}
+            </PrimaryButton>
+            <Divider orientation="vertical" />
+            <Button
+              variant="link"
+              variantColor="brandBlue"
+              fontWeight={500}
               onClick={() => {
                 dispatch({type: 'SET_URL', data: BASE_API_URL})
                 saveExternalUrl(BASE_API_URL)
@@ -225,83 +215,64 @@ function NodeSettings() {
               }}
             >
               {t('Use default')}
-            </FlatButton>
-          </Flex>
-          <Flex align="center" css={{marginTop: 10}}>
-            <Label htmlFor="key" style={{width: 120}}>
-              {`${t('Node api key')} `}
-            </Label>
-            <Box style={{position: 'relative'}}>
+            </Button>
+          </Stack>
+          <Stack isInline spacing={2} align="center">
+            <FormLabel htmlFor="key" fontWeight={400} minW={24}>
+              {t('Node api key')}
+            </FormLabel>
+            <InputGroup>
               <Input
                 id="key"
                 value={state.apiKey}
                 type={revealApiKey ? 'text' : 'password'}
+                w="xs"
                 onChange={e =>
                   dispatch({type: 'SET_API_KEY', data: e.target.value})
                 }
-                style={{
-                  ...margin(0, theme.spacings.normal, 0, theme.spacings.small),
-                  width: rem(300),
-                }}
               ></Input>
-              <Box
-                style={{
-                  background: theme.colors.gray2,
-                  ...borderRadius('right', rem(6)),
-                  cursor: 'pointer',
-                  fontSize: rem(20),
-                  position: 'absolute',
-                  ...padding(0, rem(8)),
-                  top: 0,
-                  height: '100%',
-                  right: '12px',
-                }}
-                onClick={() => setRevealApiKey(!revealApiKey)}
-              >
-                {revealApiKey ? (
-                  <FiEyeOff style={{transform: 'translate(0, 50%)'}} />
-                ) : (
-                  <FiEye style={{transform: 'translate(0, 50%)'}} />
-                )}
-              </Box>
-            </Box>
-            <Button
+              <InputRightElement h="full">
+                <IconButton
+                  size="xs"
+                  icon={revealApiKey ? FiEyeOff : FiEye}
+                  bg="white"
+                  _hover={null}
+                  onClick={() => setRevealApiKey(!revealApiKey)}
+                />
+              </InputRightElement>
+            </InputGroup>
+            <PrimaryButton
               onClick={() => {
                 saveExternalApiKey(state.apiKey)
                 notify()
               }}
             >
               {t('Save')}
-            </Button>
-          </Flex>
-        </Box>
+            </PrimaryButton>
+          </Stack>
+        </Stack>
       )}
       {!settings.useExternalNode && (
-        <div>
-          <SubHeading
-            css={margin(theme.spacings.medium24, 0, theme.spacings.medium16, 0)}
-          >
+        <Box>
+          <Heading fontWeight={500} fontSize="lg" mb={4}>
             {t('Built-in node log')}
-          </SubHeading>
-          <div
+          </Heading>
+          <Flex
             ref={logsRef}
             direction="column"
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              height: 300,
-              overflow: 'auto',
-              wordWrap: 'break-word',
-              border: `1px solid ${theme.colors.muted}`,
-            }}
+            height="xs"
+            overflow="auto"
+            wordBreak="break-word"
+            borderColor="muted"
+            borderWidth="px"
           >
             {state.logs.map((log, idx) => (
               <Ansi key={idx}>{log}</Ansi>
             ))}
-          </div>
-        </div>
+          </Flex>
+        </Box>
       )}
-    </SettingsLayout>
+    </Stack>
   )
 }
 
