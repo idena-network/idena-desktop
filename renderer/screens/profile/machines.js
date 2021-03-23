bc485a968f956644462d28367f878c25f694ed0a
aidenaio <a@idena.io>
2021-03-23 19:47:28 +0500

    Handle delegation / mining activation cases

--- renderer/screens/profile/machines.js
+++ renderer/screens/profile/machines.js
@@ -1,31 +1,60 @@
 import {assign, createMachine} from 'xstate'
+import {log} from 'xstate/lib/actions'
 import {HASH_IN_MEMPOOL} from '../../shared/hooks/use-tx'
+import {NodeType} from '../../shared/types'
 import {callRpc} from '../../shared/utils/utils'
 
 export const activateMiningMachine = createMachine({
   id: 'mining',
+  context: {
+    mode: NodeType.Miner,
+  },
   initial: 'idle',
   states: {
     idle: {
       on: {
-        ACTIVATE: 'activating',
+        SHOW: 'showing',
       },
     },
-    activating: {
-      initial: 'preview',
+    showing: {
+      initial: 'idle',
       states: {
-        preview: {
+        idle: {
           on: {
-            ACTIVATE: 'submitting',
+            CHANGE_MODE: {
+              actions: [
+                assign({
+                  mode: (_, {mode}) => mode,
+                }),
+                log(),
+              ],
+            },
+            ACTIVATE: 'activating',
+            DEACTIVATE: 'deactivating',
           },
         },
-        submitting: {
+        activating: {
           invoke: {
-            src: ({isOnline}, {delegatee}) =>
-              callRpc(
-                isOnline ? 'dna_becomeOffline' : 'dna_becomeOnline',
-                delegatee
-              ),
+            src: ({mode}, {delegatee}) =>
+              mode === NodeType.Delegator
+                ? callRpc('dna_delegate', {to: delegatee})
+                : callRpc('dna_becomeOnline'),
+            onDone: {
+              target: 'mining',
+              actions: [
+                assign({
+                  hash: (_, {data}) => data,
+                }),
+              ],
+            },
+          },
+        },
+        deactivating: {
+          invoke: {
+            src: ({mode}) =>
+              mode === NodeType.Delegator
+                ? callRpc('dna_undelegate')
+                : callRpc('dna_becomeOffline'),
             onDone: {
               target: 'mining',
               actions: [
