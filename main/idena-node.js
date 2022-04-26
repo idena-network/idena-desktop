6e944f482e860c500814e85f5025601955ad55f6
aidenaio <a@idena.io>
2022-04-27 01:11:46 +0500

    Fix built-in node spawn args

--- main/idena-node.js
+++ main/idena-node.js
@@ -131,14 +131,12 @@ async function startNode(
     tcpPort,
     '--ipfsport',
     ipfsPort,
-    '--autoonline',
-    autoActivateMining,
   ]
 
   const version = await getCurrentVersion(false)
-  if (version > '0.14.3') {
-    parameters.push('--apikey')
-    parameters.push(apiKey)
+
+  if (autoActivateMining && semver.gt(version, '0.28.3')) {
+    parameters.push('--autoonline')
   }
 
   const configFile = getNodeConfigFile()
