209bc16aedf9eb1859ccfdad4bcd96674fb93ec7
ridenaio <r@idena.io>
2022-08-23 14:22:17 +0500

    Remove ability to kill newbie invite

--- renderer/screens/contacts/utils.js
+++ renderer/screens/contacts/utils.js
@@ -3,6 +3,4 @@ import {IdentityStatus} from '../../shared/types'
 export const canKill = (knownIdentity, persistedIdentity) =>
   persistedIdentity?.state === IdentityStatus.Invite ||
   (Boolean(knownIdentity) &&
-    [IdentityStatus.Newbie, IdentityStatus.Candidate].includes(
-      persistedIdentity?.state
-    ))
+    persistedIdentity?.state === IdentityStatus.Candidate)
