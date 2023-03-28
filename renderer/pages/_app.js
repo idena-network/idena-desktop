6abbcefc6cfcc932da6552eb7303f38b671cf5f8
aidenaio <50833934+aidenaio@users.noreply.github.com>
2023-03-28 14:14:19 +0500

    Disable Change keywords button during the fetch (#898)
    * Disable Change keywords button during the fetch
    
    * Apply disabling for edit flip page
    
    * Change var name

--- renderer/pages/_app.js
+++ renderer/pages/_app.js
@@ -6,7 +6,7 @@ import GoogleFonts from 'next-google-fonts'
 // eslint-disable-next-line import/no-extraneous-dependencies
 import 'tui-image-editor/dist/tui-image-editor.css'
 import '../i18n'
-import {QueryClient, QueryClientProvider} from 'react-query'
+import {QueryClientProvider} from 'react-query'
 import {theme} from '../shared/theme'
 import {NodeProvider} from '../shared/providers/node-context'
 import {SettingsProvider} from '../shared/providers/settings-context'
@@ -17,6 +17,7 @@ import {EpochProvider} from '../shared/providers/epoch-context'
 import {IdentityProvider} from '../shared/providers/identity-context'
 import {VotingNotificationProvider} from '../shared/providers/voting-notification-context'
 import {OnboardingProvider} from '../shared/providers/onboarding-context'
+import {queryClient} from '../shared/utils/utils'
 
 // err is a workaround for https://github.com/zeit/next.js/issues/8592
 export default function App({Component, err, ...pageProps}) {
@@ -35,8 +36,6 @@ export default function App({Component, err, ...pageProps}) {
     </>
   )
 }
-
-const queryClient = new QueryClient()
 
 function AppProviders(props) {
   return (
--- renderer/pages/flips/edit.js
+++ renderer/pages/flips/edit.js
@@ -316,7 +316,10 @@ export default function EditFlipPage() {
                     <FlipStoryAside>
                       <IconButton2
                         icon={<RefreshIcon />}
-                        isDisabled={availableKeywords.length === 0}
+                        isDisabled={
+                          availableKeywords.length === 0 ||
+                          is('keywords.loading')
+                        }
                         onClick={() => send('CHANGE_KEYWORDS')}
                       >
                         {t('Change words')}
@@ -419,7 +422,8 @@ export default function EditFlipPage() {
                 is('images.painting') ||
                 is('protect.protecting') ||
                 is('protect.shuffling') ||
-                is('protect.preparing')
+                is('protect.preparing') ||
+                is('keywords.loading')
               }
               onClick={() => send('NEXT')}
             >
